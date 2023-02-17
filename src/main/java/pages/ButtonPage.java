778b098149564f1d18a7a3bc3fd510f9b0cce5fb
AnnaNezhevets <nuta2009kr@gmail.com>
2023-02-17 14:46:21 +0300

    part 2

--- src/main/java/pages/ButtonPage.java
+++ src/main/java/pages/ButtonPage.java
@@ -1,9 +1,17 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class ButtonPage {
+public class ButtonPage extends BasePage{
     public ButtonPage(WebDriver driver) {
+        super(driver);
+    }
 
+    private final By buttonButtons = By.xpath("//span[text() = 'Buttons']");
+
+    public ButtonPage enterButtons() {
+        driver.findElement(buttonButtons).click();
+        return this;
     }
 }
--- src/test/java/tests/Button_test.java
+++ src/test/java/tests/Button_test.java
@@ -1,4 +1,14 @@
 package tests;
 
-public class Button_test {
+import org.junit.jupiter.api.Test;
+import tests.base.BaseTest;
+
+import static constants.Constains.Urls.DEMOQA;
+
+public class Button_test extends BaseTest {
+    @Test
+        public void testOpenButtons(){
+        basePage.goToUrl(DEMOQA);
+        buttonPage.enterButtons();
+    }
 }
