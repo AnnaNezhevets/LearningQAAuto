ab0f46d03fb0f077211bcbfc28c095416066c225
AnnaNezhevets <nuta2009kr@gmail.com>
2023-03-21 20:55:19 +0300

    try2

--- src/test/java/tests/base/BaseTest.java
+++ src/test/java/tests/base/BaseTest.java
@@ -33,8 +33,8 @@ public class BaseTest {
             javascriptExecutor.executeScript("window.sessionStorage.clear()");
         }
     }
-    @AfterAll
-    void close(){
-        driver.close();
-   }
+//    @AfterAll
+//    void close(){
+//        driver.close();
+//   }
 }
